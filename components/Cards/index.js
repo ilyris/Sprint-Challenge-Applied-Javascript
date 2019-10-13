983ecbcc3d4ba8808e26eb17d8ce54eed3cbc6af
Dylan Sieren <dylan.sieren01@gmail.com>
2019-10-13 18:39:23 -0500

    believe i have finished the card section, which should be the last of my homework

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,7 +1,7 @@
 // STEP 3: Create Article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Stduy the response data you get back, closely.
+// Study the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
@@ -17,3 +17,55 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+.then( response => {
+    console.log(response.data.articles);
+    const articles = response.data.articles;
+    const cardsWrapper = document.querySelector(".cards-container");
+
+    function ArticleComponents(object) {
+        const cardContainer = document.createElement('div');
+        const headline = document.createElement('div');
+        const author = document.createElement('div');
+        const imageContainer = document.createElement("div");
+        const image = document.createElement("img");
+        const span = document.createElement("span");
+
+        cardContainer.appendChild(headline);
+        cardContainer.appendChild(author);
+        author.appendChild(imageContainer);
+        imageContainer.appendChild(image);
+        author.appendChild(span);
+
+        cardContainer.classList.add("card");
+        headline.classList.add("headline");
+        author.classList.add("author");
+        imageContainer.classList.add("img-container");
+
+        headline.textContent = `${object.headline}`;
+        image.src = `${object.authorPhoto}`;
+        span.textContent = `${object.authorName}`;
+
+        return cardContainer;
+    }
+
+    articles.javascript.forEach( javascriptArticles => {
+        return cardsWrapper.appendChild(ArticleComponents(javascriptArticles));
+    });
+    articles.bootstrap.forEach( bootstrapArticles => {
+        return cardsWrapper.appendChild(ArticleComponents(bootstrapArticles));
+    });
+    articles.jquery.forEach( jqueryArticles => {
+        return cardsWrapper.appendChild(ArticleComponents(jqueryArticles));
+    });
+    articles.node.forEach( nodeArticles => {
+        return cardsWrapper.appendChild(ArticleComponents(nodeArticles));
+    });
+    articles.technology.forEach( technologyArticles => {
+        return cardsWrapper.appendChild(ArticleComponents(technologyArticles));
+    });
+})
+.catch(error => {
+    return error;
+})
